598f834924b0863b07ed96a2a4522194397f7652
Bader Alsabah <bhyss@hypertext.com.kw>
2022-06-14 12:16:23 +0300

    added first task and a coding solution for it

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## CODED - React State - Code Task
 
+### TASK 1 - The Forbidden Task!
+
+We have discussed how changing `state` directly is not permitted. Let us start by experimenting what happens to our code if we change state directly through an assginemnt operator. This often referred to as `mutating state directly`.
+
+1 - On line 30 in the click handler - remove `this.setState` method and replace it with a direct assignment operator in this form `this.state.pushups`.
+
+2 - Save your changes and reload the the App. What do you notice happens as you click the button below the pushup picture `1 set completed`. Does the number of pushups sets displayed change?
+
+3 - Is state being modified or not? To check this - add a `console.log` statement and log `this.state.pushups` to the console. Does it appear to be updating when the button is clicked or not? Analyze what's happening in the App and describe in your words what is happening (this is a good time to do some reasearch and view documenation).
+
 1 - To get started - navigate to the project directory from your terminal/command line interface and type the following command : `npm start`
 
 2 - Open VS Code or your favorite code editor and then open the entire directory as a project folder
---
+++ src/images/solutions/App-solution-1.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import pushupImg from "./images/pushup.jpeg";
+import situpImg from "./images/situp.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <MyWorkout name="Bader" />
+    </div>
+  );
+}
+
+class MyWorkout extends React.Component {
+  state = { pushups: 6, situps: 4 };
+  render() {
+    return (
+      <>
+        <h1>Hello {this.props.name}</h1>
+        <h2>Your Workout Tracker</h2>
+        <img
+          src={pushupImg}
+          alt="man-doing-pushups"
+          height="550"
+          width="800"
+        ></img>
+        <h3>{this.state.pushups} Pushup Sets Remaining</h3>
+        <button
+          onClick={() => {
+            this.state.pushups = this.state.pushups - 1;
+            console.log(this.state.pushups);
+          }}
+          style={{ marginBottom: "1em" }}
+        >
+          1 Set Completed
+        </button>
+        <br></br>
+        <img
+          src={situpImg}
+          alt="woman-doing-situp"
+          height="550"
+          width="800"
+        ></img>
+        <h3>{this.state.situps} Situp Sets Remaining</h3>
+        <button
+          onClick={() => {
+            this.setState((prevState) => ({ situps: prevState.situps - 1 }));
+          }}
+          style={{ marginBottom: "3em" }}
+        >
+          1 Set Completed
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
